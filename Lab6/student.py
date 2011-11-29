015aa6253fe93800dc1fb42761ddd9de879f6782
Mike Friend <sodium56@gmail.com>
2011-11-29 23:19:47 +0000

    almost done w individual

--- Lab6/student.py
+++ Lab6/student.py
@@ -1,27 +1,24 @@
 class Student(object):
-	def __init__(self, first, last):
-		self.first = first
-		self.last = last
-		self.test = []
-		self.lab = []
+    def __init__(self, first, last):
+        self.first = first
+	self.last = last
+	self.test = []
+	self.lab = []
 
-	def add_test(self, test):
-        """
-        Adds to the test list
-        """
-		self.test.append(test)
+    def add_test(self, test):
+        
+        #Adds to the test list
+        
+	 self.test.append(test)
  
-	def add_lab(self,lab):
-        """
-        Adds to the lab list
-        """
-		self.lab.append(lab)
-
-	def grade(self, labw):
-        """
-        Takes in the lab weight
-        """
-		labav = sum(self.lab) / len(self.lab)
-		testav =sum(self.test) / len(self.test)
-		return (labw * labav) + ((1 - labw) * testav)
+    def add_lab(self,lab):
+       
+        #Adds to the lab list
+        
+	self.lab.append(lab)
+    def grade(self, labw):
+       labw=float(labw) 
+       labav = sum(self.lab) / len(self.lab)
+       testav =sum(self.test) / len(self.test)
+       return (labw * labav) + ((1 - labw) * testav)
 
--- Lab6/test.py
+++ Lab6/test.py
@@ -4,59 +4,65 @@ f = open('grades.txt', 'r')
 # read the first line
 var = f.readline()
 vlist = var.split(',')
-print vlist
+
 num_lab = vlist[0]
 num_test = vlist[1]
 labw = vlist[2]
 studenttest = Student('Mike','Friend')
 studentlist = []
-studentreal = False
+
 
 
 for line in f:
     newline = line.split('\t')
     first = newline[0]
     last = newline[1]
-    grade = newline[2]
-    type = newline[3]
-    studentreal = False
+    grade =int(newline[2])
+    type = newline[3].lstrip()
+    type=type.rstrip()	
+   # print first+":"+last
+   # print grade
+   # print type
+    studentreal=False
     for item in studentlist:
    
         if first == item.first and last == item.last: #tests if the student object already exists
             studentreal=True
-            if type == 'exam':
-                print "added test to current"
+            if len(type) == 4:
+               # print item.first+":"+str(grade)+":"+"added test to current"
                 item.add_test(grade)
             else:	
-                print "added lab to current"
+             #   print "added lab to current"
                 item.add_lab(grade)
 
     if not studentreal:
         tempstudent=Student(first,last)    #creates a new student object
-        if type == 'exam':
-            print "added test to new"
-            tempstudent.add_exam(grade)
+        if len(type) == 4:
+           # print item.first+":"+":"+str(grade)+":"+"added test to new"
+            tempstudent.add_test(grade)
         else:
-            print "added lab to new"
+          #  print "added lab to new"
             tempstudent.add_lab(grade)	
-	    studentlist.append(tempstudent)         #adds the new student to the list
-		
-sortedlist=sorted(studentlist, key=lambda Student: Student.last)#sorts by last name
+	studentlist.append(tempstudent)         #adds the new student to the list
 
+
+#sorts the list by last name
+sortedlist=sorted(studentlist, key=lambda Student: Student.last)
+#prints out the sorted list
 for item in sortedlist:
-    print item.first+":"+item.last+":"
-    grade=item.grade(Labw)
-    letter=z
+    grade=item.grade(labw)
+  #this assigns a letter based on a number
+    letter='z'
     if grade > 50:
-        letter = F
+        letter = 'F'
     if grade > 60:
-       letter = D
+       letter = 'D'
     if grade > 70:
-       letter = C
+       letter = 'C'
     if grade > 80:
-       letter = B
+       letter = 'B'
     if grade > 90:
-       letter = A
-    print letter
+       letter = 'A'
+    print item.first+":"+item.last+":"+letter
 
 
